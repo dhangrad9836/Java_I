7f02196628786f6cd14a2c5984bc6592da36d5a1
Darren Dhanpat <darren.dhanpat@yahoo.com>
2022-08-08 23:21:17 -0400

    Initial Commit

---
+++ SectionTwo/Bus.java
@@ -0,0 +1,16 @@
+public class Bus {
+    public static void main(String[] args) {
+        
+
+        int harry = -50;
+        harry += -3;
+        int hermione = 30;
+        harry += hermione;
+        int ron = 100;
+        harry += ron;
+        harry += 60;
+        System.out.println(harry);
+
+            
+    }
+}
