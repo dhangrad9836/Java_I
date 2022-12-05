2ff8261a59418b6289e819c971093296e3e7081b
Darren Dhanpat <darren.dhanpat@yahoo.com>
2022-12-05 09:57:03 -0500

    added random card draw

--- SectionEleven_BlackJack/Blackjack.java
+++ SectionEleven_BlackJack/Blackjack.java
@@ -11,8 +11,12 @@ public class Blackjack {
         System.out.println("..Ready? Press anything to begin!");
 
 
-        //test random Number
-        System.out.println(drawRandomCard() + " Hello");
+        //Return a random number from 1 - 13, store in a variable
+        int randNum = drawRandomCard();
+
+        //Return random card
+        String drawCard = cardString(randNum);
+        System.out.println(drawCard);
 
         //Task 3 – Wait for the user to press enter.
         //Task 4 – Get two random cards.
@@ -64,6 +68,116 @@ public class Blackjack {
      * Inside the function:
      *   1. Returns a String drawing of the card.
      */
+    public static String cardString(int cardNumber){
+        switch(cardNumber){
+            case 1: return 
+            
+                    "   _____\n"+
+                    "  |A _  |\n"+ 
+                    "  | ( ) |\n"+
+                    "  |(_'_)|\n"+
+                    "  |  |  |\n"+
+                    "  |____V|\n";
+
+            case 2: return
+                    "   _____\n"+              
+                    "  |2    |\n"+ 
+                    "  |  o  |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____Z|\n";
+
+            case 3: return
+                    "   _____\n" +
+                    "  |3    |\n"+
+                    "  | o o |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____E|\n";
+
+            case 4: return
+                    "   _____\n" +
+                    "  |4    |\n"+
+                    "  | o o |\n"+
+                    "  |     |\n"+
+                    "  | o o |\n"+
+                    "  |____h|\n";
+
+            case 5: return
+                    "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+
+            case 6: return
+                    "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+
+            case 7: return
+                    "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+
+            case 8: return
+                    "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+
+            case 9: return
+                    "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+
+            case 10: return
+                    "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+
+            case 11: return
+                    "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n";
+
+            case 12: return                    
+                    "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n";
+
+            case 13: return
+                    "   _____\n" +
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n";
+            default : return "This should not get called";
+        
+        }
+    } //end cardString function
 
     public static String faceDown() {
         return
